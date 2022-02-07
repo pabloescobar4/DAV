ee6fee876105ba5a75d6cf72aa96e60a91f08be3
dheerajmehta <dheeraj_fw11_310@masai.school>
2022-02-07 20:32:44 +0530

    Update App.js

--- routing/src/App.js
+++ routing/src/App.js
@@ -3,18 +3,31 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Search from "./Search"
 import Homes from "./Homes"
+import React ,{useState} from "react"
 function App() {
+
+  const [navbar,setNavbar] = useState()
+
+  const changeName = () => {
+    let val = navbar;
+    (val >24) ? 
+    setNavbar('home') : setNavbar(25) 
+  }
+
+
   return (
     <div className="App">
-      <h1>Navbar</h1>
-     <Routes>
+      <h1>{navbar}</h1>
+      <button onClick={ changeName}>home</button>
+      
+     {/* <Routes>
        <Route path="/search" element={<Search />} />
    
          
        <Route path="/home" element={<Homes/>} />
        </Routes>
        
-        
+         */}
       
 
 
