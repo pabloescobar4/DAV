b209d7d6416f5b1dded4a733b1ad7938ff654393
dheerajmehta <dheeraj_fw11_310@masai.school>
2022-03-06 12:54:37 +0530

    Update App.js

--- my-project1/src/App.js
+++ my-project1/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './Components/Navbar'
-import ProdData from './Components/prod_Data';
+// import ProdData from './Components/prod_Data';
 import Prod from './Components/prod';
 import Prod1 from './Components/prod1';
 export default function App() {
