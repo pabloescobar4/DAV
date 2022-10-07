1de3a5ef97fa6769dc763ca48c5fb3c5922fe74b
ABHISHEK SINGH <87421842+Abhisingh755@users.noreply.github.com>
2022-10-08 00:59:57 +0530

    dsa

---
+++ DSA/LeetCode/Reverse.js
@@ -0,0 +1,17 @@
+// An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  or .
+
+// Reverse an array of integers.
+
+// Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this.
+
+// Example
+
+// Return .
+
+const Reverse = () => {
+  let array = [1, 4, 3, 5];
+  let arr = array.reverse();
+
+  return arr;
+};
+console.log(Reverse());
